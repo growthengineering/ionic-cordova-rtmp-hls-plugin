c622042fd71655dfa4b0ad3e6579992876fc9550
Joao Lourenco <joao.lourenco@growthengineering.co.uk>
2023-10-23 14:06:56 +0100

    Add dependency inside build gradle file and add missing imports

--- src/android/IoniCordovaRTMPandHLS.java
+++ src/android/IoniCordovaRTMPandHLS.java
@@ -7,6 +7,21 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresPermission;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
 /**
  * This class echoes a string called from JavaScript.
  */
