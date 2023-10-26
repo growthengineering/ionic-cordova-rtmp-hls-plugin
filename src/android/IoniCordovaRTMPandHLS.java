5e5da22de869f77008ff5ed5a982fd302e63f6d8
Joao Lourenco <joao.lourenco@growthengineering.co.uk>
2023-10-26 09:15:36 +0100

    Add preview camera logic with permissions request

--- src/android/IoniCordovaRTMPandHLS.java
+++ src/android/IoniCordovaRTMPandHLS.java
@@ -1,15 +1,18 @@
 package cordova.plugin.ionicrtmphls;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.hardware.camera2.CameraCharacteristics;
 import android.os.Bundle;
 import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
 import android.Manifest;
 import android.content.pm.PackageManager;
@@ -18,6 +21,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -63,7 +67,7 @@ public class IoniCordovaRTMPandHLS extends CordovaPlugin {
 
     @Override
     public void initialize(final CordovaInterface _cordova, final CordovaWebView _webView) {
-        super.initialize(cordova, webView);
+        super.initialize(_cordova, _webView);
         webView = _webView;
         cordova = _cordova;
     }
@@ -106,37 +110,60 @@ public class IoniCordovaRTMPandHLS extends CordovaPlugin {
     }
 
     private void previewCamera(CallbackContext callbackContext) {
+        Context context = cordova.getActivity().getApplicationContext();
+        Toast.makeText(context, "previewCamera", Toast.LENGTH_SHORT).show();
         cordova.getActivity().runOnUiThread(new Runnable() {
+            @SuppressLint("ResourceAsColor")
             @Override
             public void run() {
+                Toast.makeText(context, "1 Permissions", Toast.LENGTH_SHORT).show();
                 // Ensure you have the necessary permissions
                 requestPermissions(callbackContext);
 
-                // Initialize RTMP connection and stream
-                RtmpConnection connection = new RtmpConnection();
-                RtmpStream stream = new RtmpStream(connection);
+                Toast.makeText(context, "2 After Permissions", Toast.LENGTH_SHORT).show();
 
-                Context context = cordova.getActivity().getApplicationContext();
+                // Initialize RTMP connection and stream
+
+               // RtmpConnection connection = new RtmpConnection();
+               // RtmpStream stream = new RtmpStream(connection);
+
+
 
                 // Attach audio and video sources
-                stream.attachAudio(new AudioRecordSource(context, true));
+               // stream.attachAudio(new AudioRecordSource(context, true));
                 Camera2Source cameraSource = new Camera2Source(context, true);
                 cameraSource.open(CameraCharacteristics.LENS_FACING_BACK);
-                stream.attachVideo(cameraSource);
-
+             //   stream.attachVideo(cameraSource);
+                Toast.makeText(context, "3 ", Toast.LENGTH_SHORT).show();
                 HkSurfaceView cameraView = new HkSurfaceView(cordova.getActivity());
-                cameraView.attachStream(stream);
-
+              //  cameraView.attachStream(stream);
+                Toast.makeText(context, "4 ", Toast.LENGTH_SHORT).show();
                 ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(
                         ViewGroup.LayoutParams.MATCH_PARENT,
                         ViewGroup.LayoutParams.MATCH_PARENT
                 );
                 cameraView.setLayoutParams(layoutParams);
-
+                Toast.makeText(context, "5 ", Toast.LENGTH_SHORT).show();
+                ViewGroup parentView = (ViewGroup) webView.getView().getParent();
+                if (parentView instanceof FrameLayout) {
+                    Toast.makeText(context, "5.1 ", Toast.LENGTH_SHORT).show();
+                    FrameLayout frameLayout = (FrameLayout) parentView;
+                    frameLayout.addView(cameraView, layoutParams);
+                    frameLayout.bringChildToFront(cameraView);
+                } else {
+                    Toast.makeText(context, "5.2 ", Toast.LENGTH_SHORT).show();
+                    // If the parent view is not a FrameLayout, just add the cameraView directly to the activity
+                    cordova.getActivity().addContentView(cameraView, layoutParams);
+                }
+                Toast.makeText(context, "6 ", Toast.LENGTH_SHORT).show();
                 // Add the camera view to your Cordova WebView
-                cordova.getActivity().addContentView(cameraView, layoutParams);
-
-
+                webView.getView().setBackgroundColor(Color.TRANSPARENT);
+                cameraView.bringToFront();
+                Toast.makeText(context, "7 ", Toast.LENGTH_SHORT).show();
+                cameraView.setTranslationZ(1.0f);
+                cameraView.setVisibility(View.VISIBLE);
+                webView.getView().setTranslationZ(-1.0f);
+                Toast.makeText(context, "8 ", Toast.LENGTH_SHORT).show();
                 // Optionally, return success to the JavaScript side
                 callbackContext.success("Camera preview started!");
             }
