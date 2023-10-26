212655e2baa0852a4b48cf409c72b6fb150a342b
Joao Lourenco <joao.lourenco@growthengineering.co.uk>
2023-10-26 15:34:57 +0100

    add permissions for iOS in the plugin xml, fix ios building issue and added request permissiosn logic

--- src/ios/IoniCordovaRTMPandHLS.m
+++ src/ios/IoniCordovaRTMPandHLS.m
@@ -33,37 +33,70 @@
 
 - (void)previewCamera:(CDVInvokedUrlCommand*)command
 {
-    CDVPluginResult* pluginResult  [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:"previewCamera Executed!"];
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"previewCamera Executed!"];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 - (void)swapCamera:(CDVInvokedUrlCommand*)command
 {
-    CDVPluginResult* pluginResult  [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:"swapCamera Executed!"];
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"swapCamera Executed!"];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 - (void)startBroadcasting:(CDVInvokedUrlCommand*)command
 {
-    CDVPluginResult* pluginResult  [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:"startBroadcasting Executed!"];
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"startBroadcasting Executed!"];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 - (void)stopBroadcasting:(CDVInvokedUrlCommand*)command
 {
-    CDVPluginResult* pluginResult  [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:"stopBroadcasting Executed!"];
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"stopBroadcasting Executed!"];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 - (void)viewLiveStream:(CDVInvokedUrlCommand*)command
 {
-    CDVPluginResult* pluginResult  [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:"viewLiveStream Executed!"];
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"viewLiveStream Executed!"];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 - (void)requestPermissions:(CDVInvokedUrlCommand*)command
 {
-    CDVPluginResult* pluginResult  [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:"requestPermissions Executed!"];
+    // Check for camera permission
+    AVAuthorizationStatus cameraStatus = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
+    // Check for microphone permission
+    AVAuthorizationStatus microphoneStatus = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeAudio];
+    
+    // If both permissions are granted, return success
+    if (cameraStatus == AVAuthorizationStatusAuthorized && microphoneStatus == AVAuthorizationStatusAuthorized) {
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Permissions granted"];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        return;
+    }
+    
+    // If not, request the necessary permissions
+    NSMutableArray *permissionsToRequest = [NSMutableArray array];
+    if (cameraStatus == AVAuthorizationStatusNotDetermined) {
+        [permissionsToRequest addObject:AVMediaTypeVideo];
+    }
+    if (microphoneStatus == AVAuthorizationStatusNotDetermined) {
+        [permissionsToRequest addObject:AVMediaTypeAudio];
+    }
+    
+    for (NSString *mediaType in permissionsToRequest) {
+        [AVCaptureDevice requestAccessForMediaType:mediaType completionHandler:^(BOOL granted) {
+            if (!granted) {
+                // If any permission is denied, return failure
+                CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Permission denied"];
+                [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+                return;
+            }
+        }];
+    }
+    
+    // Return success if permissions were granted or if they were already granted
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Permissions granted"];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
