2d7c0c11b470dab615a4a74c44ce4bb11f0a8106
Joao Lourenco <joao.lourenco@growthengineering.co.uk>
2024-05-09 13:16:32 +0100

    add missing methods

--- src/android/IoniCordovaRTMPandHLS.java
+++ src/android/IoniCordovaRTMPandHLS.java
@@ -263,6 +263,16 @@ public class IoniCordovaRTMPandHLS extends CordovaPlugin {
 
                     player.addListener(new Player.Listener() {
                         @Override
+                        public void onCue(@NonNull Cue cue) {
+
+                        }
+
+                        @Override
+                        public void onDurationChanged(long l) {
+
+                        }
+
+                        @Override
                         public void onStateChanged(@NonNull Player.State state) {
 
                         switch (state) {
